5a0b699491275d9cbb64a8f1119737ddec44e2c4
Risda Tamam Aljava <risda123tamam@student.esaunggul.ac.id>
2022-04-30 07:28:47 +0700

    Update .env-template

--- index.js
+++ index.js
@@ -40,48 +40,6 @@ function getPrices() {
       console.log("Error at api-mainnet.magiceden.dev data:", err)
     );
 
-  // API for price data.
-  // axios
-  //   .get(
-  //     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`
-  //   )
-  //   .then((res) => {
-  //     // If we got a valid response
-  //     if (
-  //       res.data &&
-  //       res.data[0].current_price &&
-  //       res.data[0].price_change_percentage_24h
-  //     ) {
-  //       let currentPrice = res.data[0].current_price || 0; // Default to zero
-  //       let priceChange = res.data[0].price_change_percentage_24h || 0; // Default to zero
-  //       let symbol = res.data[0].symbol || "?";
-  //       client.user.setPresence({
-  //         game: {
-  //           // Example: "Watching -5,52% | BTC"
-  //           name: `${priceChange.toFixed(2)}% | ${symbol.toUpperCase()}`,
-  //           type: 3, // Use activity type 3 which is "Watching"
-  //         },
-  //       });
-
-  //       client.guilds
-  //         .find((guild) => guild.id === process.env.SERVER_ID)
-  //         .me.setNickname(
-  //           `FP: ${currentPrice
-  //             .toLocaleString()
-  //             .replace(/,/g, process.env.THOUSAND_SEPARATOR)}${
-  //             process.env.CURRENCY_SYMBOL
-  //           }`
-  //         );
-
-  //       console.log("Updated price to", currentPrice);
-  //     } else
-  //       console.log(
-  //         "Could not load player count data for",
-  //         process.env.COIN_ID
-  //       );
-  //   })
-  //   .catch((err) => console.log("Error at api.coingecko.com data:", err));
-
 }
 
 // Runs when client connects to Discord.
